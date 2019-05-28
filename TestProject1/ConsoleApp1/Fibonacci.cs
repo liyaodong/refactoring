4bf50d91686458cca91f7e8736ab546058ccf245
Yaodong Li <mail@liyaodong.com>
2019-05-29 00:09:51 +0800

    Init Project Basic

---
+++ TestProject1/ConsoleApp1/Fibonacci.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace ConsoleApp1
+{
+    public class Fibonacci
+    {
+        public static void Main()
+        {
+            Console.WriteLine("This is main function");
+        }
+        public static int Print(int n)
+        {
+            return n;
+        }
+    }
+}
